9f49d67a9de3a1c3073797171f420c8f4b98e5cb
이유창 <leeyucha@ucware.net>
2023-11-07 00:30:01 +0900

    fixed lint error

--- src/components/AvatarWithStatus.tsx
+++ src/components/AvatarWithStatus.tsx
@@ -1,26 +1,25 @@
-import * as React from 'react';
-import Badge from '@mui/joy/Badge';
-import Avatar, { AvatarProps } from '@mui/joy/Avatar';
+import Avatar, { AvatarProps } from "@mui/joy/Avatar";
+import Badge from "@mui/joy/Badge";
 
 type AvatarWithStatusProps = AvatarProps & {
-  online?: boolean;
+	online?: boolean;
 };
 
 export default function AvatarWithStatus({
-  online = false,
-  ...rest
+	online = false,
+	...rest
 }: AvatarWithStatusProps) {
-  return (
-    <div>
-      <Badge
-        color={online ? 'success' : 'neutral'}
-        variant={online ? 'solid' : 'soft'}
-        size="sm"
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        badgeInset="4px 4px"
-      >
-        <Avatar size="sm" {...rest} />
-      </Badge>
-    </div>
-  );
+	return (
+		<div>
+			<Badge
+				color={online ? "success" : "neutral"}
+				variant={online ? "solid" : "soft"}
+				size="sm"
+				anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+				badgeInset="4px 4px"
+			>
+				<Avatar size="sm" {...rest} />
+			</Badge>
+		</div>
+	);
 }
